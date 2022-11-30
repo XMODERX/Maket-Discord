dfe7f5be202e1b5a50cdb4a4e301bddf37caf3dd
LAPTOP-EGQR37BJ\User <ashyrovsasha18@icloud.com>
2022-11-30 14:12:59 +0500

    123

--- js/main.js
+++ js/main.js
@@ -1,19 +1,11 @@
 $(function () {
     var link = $('#defult-discord-button');
-    link.mouseover(function () {
-        $('nav ul li:nth-of-type(1) span').addClass('middle-white-left-line');
-    })
-    link.mouseleave(function () {
-        $('nav ul li:nth-of-type(1) span').removeClass('middle-white-left-line');
-    })
-    link.click(function () {
-        $('#defult-discord-button span').addClass('large-white-left-line');
-    })
-    $(function () {
-        del.click(function () {
-            if ($("nav ul li").hasClass("large-white-left-line")) {
-                $('nav ul #defult-discord-button span').removeClass('large-white-left-line');
-            }
-        });
-    })
+    link.hover(
+        (function () {
+            $('nav ul li:nth-of-type(1) span').addClass('middle-white-left-line');
+        }),
+        (function () {
+            $('nav ul li:nth-of-type(1) span').removeClass('middle-white-left-line');
+        })
+    );
 });
