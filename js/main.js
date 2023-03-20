d8335ef85bd9a9c34d871f1e8e2e8c0f23e123d7
DESKTOP-JDS0IKJ\Егор <sekboyego111@gmail.com>
2023-03-20 15:24:36 +0500

    починил бездарю, js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 $(function () {
+    var link = $('nav ul li .defult-button-inner');
     var link = $('#defult-discord-button');
-    link.hover(
-        (function () {
-            $('nav ul li:nth-of-type(1) span').addClass('middle-white-left-line');
-        }),
-        (function () {
-            $('nav ul li:nth-of-type(1) span').removeClass('middle-white-left-line');
-        })
-    );
-});
+    link.mouseover(function () {
+        $('nav ul li:nth-of-type(1) span').addClass('middle-white-left-line');
+    })
+    link.mouseleave(function () {
+        $('nav ul li:nth-of-type(1) span').removeClass('middle-white-left-line');
+    })
+    link.click(function () {
+        $('nav ul #defult-discord-button span').addClass('large-white-left-line');
+        $('#defult-discord-button span').addClass('large-white-left-line');
+    link.dblclick(function () {
+        $('nav ul #defult-discord-button span').removeClass('large-white-left-line');
+        $('#defult-discord-button span').removeClass('large-white-left-line');
+    });})})
